382c5ac21e2e89e13415ff30c5b071f8440cacbd
Seltikc <seltikc@yandex.ru>
2022-07-04 17:45:20 +0500

    ÐТретий урок готов!

--- src/index.js
+++ src/index.js
@@ -2,3 +2,46 @@ import './index.html';
 import './card.html';
 import './cart.html';
 import './index.scss';
+
+
+import Swiper, {Thumbs, Scrollbar} from 'swiper';
+
+import 'swiper/css';
+import 'swiper/css/scrollbar';
+import  'swiper/css/pagination';
+
+const thumbSwiper = new Swiper('.card__slider-thumb', {
+    spaceBetween: 44,
+    slidesPerView: 3,
+    scrollbar: {
+        el: '.swiper-scrollbar',
+        draggable: true,
+    },
+    modules: [Scrollbar]
+
+});
+
+new Swiper('.card__image', {
+    spaceBetween: 10,
+    slidesPerView: 1,
+    thumbs: {
+        swiper: thumbSwiper,
+        slideThumbActiveClass: 'card__thumb-btn_active',
+    },
+    modules: [Thumbs]
+
+});
+
+new Swiper('.recommended__carousel', {
+    spaceBetween: 30,
+    slidesPerView: 5,
+        pagination: {
+            el: ".swiper-pagination",
+            type: "progressbar",
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+
+});
